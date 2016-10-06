46ed7ab893e54ef67909af937519789494af81a0
Juan Antonio Suarez <jasrsir@gmail.com>
2016-10-06 09:19:28 +0200

    Initial Version Login Relative

---
+++ app/src/main/java/com/jasrsir/loginrelative/LoginRelative.java
@@ -0,0 +1,13 @@
+package com.jasrsir.loginrelative;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginRelative extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_relative);
+    }
+}
